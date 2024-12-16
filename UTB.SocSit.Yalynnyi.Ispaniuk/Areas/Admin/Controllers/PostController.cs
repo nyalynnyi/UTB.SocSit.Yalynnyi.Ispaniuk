973fc14a2c22ce53a75fd446e20f9911c955d443
danyilispaniuk <danulispanyk@gmail.com>
2024-12-16 15:55:20 +0100

    admin area

---
+++ UTB.SocSit.Yalynnyi.Ispaniuk/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.SocSit.Yalynnyi.Ispaniuk.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UTB.SocSit.Yalynnyi.Ispaniuk/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.SocSit.Yalynnyi.Ispaniuk.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
